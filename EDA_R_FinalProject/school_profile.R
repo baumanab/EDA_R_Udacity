a10feb0624878d8ea5d4f3b118c658c466ab8978
baumanab <baumanab@gmail.com>
2015-02-27 22:30:39 -0800

    Added WA OSPI Public and Private School listings .txt, removed school_profile function

--- EDA_R_FinalProject/school_profile.R
+++
@@ -1,8 +0,0 @@
-#function accepts a dataframe (key) and a lookup value (query) and returns school info
-school_profile <- function(key,query) {
-  #check if query in key
-  
-  #return values
-  
-  #return NULL
-}
